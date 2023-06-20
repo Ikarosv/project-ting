7aa5202379a3050236d92a5be753d702d53ac136
Ikarosv <ikarovieiraa@gmail.com>
2023-06-20 15:50:05 -0300

    ✨ feat: classe de fila implementada

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        try:
+            if index < 0:
+                raise
+            return self._queue[index]
+        except:
+            raise IndexError("Índice Inválido ou Inexistente")
